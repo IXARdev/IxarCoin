967d61bf2a5f1ff6f8fcd28779a99a01ccbeb9d6
IXARdev <olddeath78@yahoo.com>
2018-06-28 23:12:34 -0400

    Initialize IXAR Blockchain

---
+++ README.md
@@ -0,0 +1,70 @@
+
+## [Join us on Discord](https://discord.gg/3Bjhz4k)
+
+
+# Ixarcoin
+
+We introduce you a new cryptocoin called Ixarcoin.
+Ixarcoin is a safe, anonymous, fast and highly profitable coin with Masternodes
+Welcome to the future!
+
+## Main futures:
+
+*	ASIC Resistance
+*	NiceHash Resistance
+*	Nvidia & AMD GPU Mining
+*	Instamine Protection
+*	Wallet Built-in Block Explorer
+*	Masternode Reward Protection
+*	Friendly Masternode Setup
+*	Long-Term Development & Support
+
+
+### Base Specs:
+
+*	Algorithm (PoW/PoS): C11/SHA256d
+*	Block Time (PoW/PoS): 1.5 minutes/1.5 minutes
+*	Maturity: 90 blocks
+*	Difficulty Retargeting: every block
+*	Total Supply: ~88,302,051 IXAR
+*	PoS coins maturation: 24 hours
+* Minimum PoS: 800 IXAR
+
+
+#### PoW Rewards:
+
+*	since block 1 : 1,580,000 (Premine)
+*	since block 2 : 5
+*	since block 1921 : 10
+*	since block 12001 : 15
+*	since block 32001 : 25
+*	since block 100001 : 32
+*	since block 138001 : 28
+* since block 210001 : 10
+* Last PoW Block = Block 350000
+
+
+##### PoS Rewards:
+
+*	since block 1921: 30
+* since block 12001: 45
+*	since block 32001 : 75
+*	since block 100001 : 128
+*	since block 138001 : 112
+*	since block 210001 : 90
+*	since block 350001 : 60
+*	since block 700001 : 40
+* since block 1050001 : 20
+*	since block 1400001 : 10
+*	since block 1750001 : 5
+* Halves every 350000 blocks after
+
+
+##### MN Rewards:
+
+* since block 1921: 90%
+
+
+###### License
+
+Ixarcoin is released under the terms of the MIT license. See COPYING for more information or see http://opensource.org/licenses/MIT.
---
+++ src/chainparams.cpp
@@ -0,0 +1,223 @@
+// Copyright (c) 2010 Satoshi Nakamoto
+// Copyright (c) 2009-2012 The Bitcoin developers
+// Copyright (c) 2014-2015 Dash Developers
+// Copyright (c) 2017-2018 The Ixarcoin developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "assert.h"
+#include "chainparams.h"
+#include "main.h"
+#include "util.h"
+#include "base58.h"
+#include <boost/assign/list_of.hpp>
+
+using namespace boost::assign;
+
+struct SeedSpec6 {
+    uint8_t addr[16];
+    uint16_t port;
+};
+
+#include "chainparamsseeds.h"
+
+//
+// Main network
+//
+
+// Convert the pnSeeds array into usable address objects.
+static void convertSeeds(std::vector<CAddress> &vSeedsOut, const unsigned int *data, unsigned int count, int port)
+{
+     // It'll only connect to one or two seed nodes because once it connects,
+     // it'll get a pile of addresses with newer timestamps.
+     // Seed nodes are given a random 'last seen time' of between one and two
+     // weeks ago.
+     const int64_t nOneWeek = 7*24*60*60;
+    for (unsigned int k = 0; k < count; ++k)
+    {
+        struct in_addr ip;
+        unsigned int i = data[k], t;
+
+        // -- convert to big endian
+        t =   (i & 0x000000ff) << 24u
+            | (i & 0x0000ff00) << 8u
+            | (i & 0x00ff0000) >> 8u
+            | (i & 0xff000000) >> 24u;
+
+        memcpy(&ip, &t, sizeof(ip));
+
+        CAddress addr(CService(ip, port));
+        addr.nTime = GetTime()-GetRand(nOneWeek)-nOneWeek;
+        vSeedsOut.push_back(addr);
+    }
+}
+
+// Hardcoded seeds.
+static void getHardcodedSeeds(std::vector<CAddress> &vSeedsOut)
+{
+    std::vector<std::string> ips;
+    const int64_t oneWeek = 7 * 24 * 60 * 60;
+    for (size_t i = 0; i < ips.size(); ++i)
+    {
+        CAddress addr(CService(ips[i], 38888));
+        addr.nTime = GetTime() - GetRand(oneWeek) - oneWeek;
+        vSeedsOut.push_back(addr);
+    }
+}
+
+class CMainParams : public CChainParams {
+public:
+    CMainParams() {
+        // The message start string is designed to be unlikely to occur in normal data.
+        // The characters are rarely used upper ASCII, not valid as UTF-8, and produce
+        // a large 4-byte int at any alignment.
+        pchMessageStart[0] = 0xa3;
+        pchMessageStart[1] = 0xfb;
+        pchMessageStart[2] = 0xdb;
+        pchMessageStart[3] = 0xdb;
+        vAlertPubKey = ParseHex("7501131e64effceb1644efc56ed3594ef64425850aa52617506177cf245575f0aa11e5b1777cfd8621ea39a7996872a07631ea25b3fdda00df37f5e982fe58850a");
+        nDefaultPort = 38888;
+        nRPCPort = 38889;
+        bnProofOfWorkLimit = CBigNum(~uint256(0) >> 16); // starting difficulty is 1 / 2^12
+
+        const char* pszTimestamp = "6/27/18 - IxarCoin Initialization";
+        std::vector<CTxIn> vin;
+        vin.resize(1);
+        vin[0].scriptSig = CScript() << 0 << CBigNum(42) << vector<unsigned char>((const unsigned char*)pszTimestamp, (const unsigned char*)pszTimestamp + strlen(pszTimestamp));
+        std::vector<CTxOut> vout;
+        vout.resize(1);
+        vout[0].SetEmpty();
+
+	// original 1516154401
+        CTransaction txNew(1, 1530148674, vin, vout, 0);
+
+        LogPrintf("genesis mainnet transaction:  %s\n", txNew.ToString().c_str());
+
+        genesis.vtx.push_back(txNew);
+
+        genesis.hashPrevBlock = 0;
+        genesis.hashMerkleRoot = genesis.BuildMerkleTree();
+        genesis.nVersion = 1;
+        genesis.nTime    = 1530148674; // Wednesday, 17-Jan-18 02:00:01 UTC
+        genesis.nBits    = bnProofOfWorkLimit.GetCompact();
+        genesis.nNonce   = 180949;
+
+        hashGenesisBlock = genesis.GetHash();
+        assert(hashGenesisBlock == uint256("0x000057ec3d9d880ccf9ac159241034ad55c4aeb810029e3c24fc45e1532be413"));
+        assert(genesis.hashMerkleRoot == uint256("0x5fbdf2bcf56b15f10ed9cf957f7543d55889b4f91e570408fa7220e0e2a79050"));
+
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,102); // i
+        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,21);
+        base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,23);
+        base58Prefixes[STEALTH_ADDRESS] = std::vector<unsigned char>(1,41);
+        base58Prefixes[EXT_PUBLIC_KEY] = list_of(0x99)(0x75)(0x45)(0xE2).convert_to_container<std::vector<unsigned char> >();
+        base58Prefixes[EXT_SECRET_KEY] = list_of(0x99)(0x73)(0x43)(0xE3).convert_to_container<std::vector<unsigned char> >();
+
+	       // NOTE: MODIFIED THIS TO 0.0.0.0
+        vSeeds.push_back(CDNSSeedData("1",  "seed.ixar.net"));
+      	vSeeds.push_back(CDNSSeedData("2",  "seed2.ixar.net"));
+      	vSeeds.push_back(CDNSSeedData("3",  "seed3.ixar.net"));
+      	vSeeds.push_back(CDNSSeedData("4",  "seed4.ixar.net"));
+      	vSeeds.push_back(CDNSSeedData("5",  "seed5.ixar.net"));
+
+      	convertSeeds(vFixedSeeds, pnSeed, ARRAYLEN(pnSeed), nDefaultPort);
+
+        getHardcodedSeeds(vFixedSeeds);
+
+        nPoolMaxTransactions = 3;
+        strDarksendPoolDummyAddress = "hFoQDUrp63QWqFhjEr3Fmc4ubHRhyzjKUC";
+
+        nLastPOWBlock = 350000;
+        nPOSStartBlock = 1920;
+    }
+
+    virtual const CBlock& GenesisBlock() const { return genesis; }
+    virtual Network NetworkID() const { return CChainParams::MAIN; }
+
+    virtual const vector<CAddress>& FixedSeeds() const {
+        return vFixedSeeds;
+    }
+protected:
+    CBlock genesis;
+    vector<CAddress> vFixedSeeds;
+};
+static CMainParams mainParams;
+
+
+//
+// Testnet
+//
+
+class CTestNetParams : public CMainParams {
+public:
+    CTestNetParams() {
+        // The message start string is designed to be unlikely to occur in normal data.
+        // The characters are rarely used upper ASCII, not valid as UTF-8, and produce
+        // a large 4-byte int at any alignment.
+        pchMessageStart[0] = 0xa4;
+        pchMessageStart[1] = 0x4c;
+        pchMessageStart[2] = 0xdc;
+        pchMessageStart[3] = 0xef;
+        bnProofOfWorkLimit = CBigNum(~uint256(0) >> 16);
+        vAlertPubKey = ParseHex("7501131a3ecd76eb1d0e4a656ed3594ef64425850aa52617506177cf245575f0aa11e5b1777cfd8621ea39a7996872a07631ea25b3fdda00df37f5e982fe58850a");
+        nDefaultPort = 38880;
+        nRPCPort = 38881;
+        strDataDir = "testnet";
+        genesis.nTime    = 1530124830;
+        genesis.nBits    = bnProofOfWorkLimit.GetCompact();
+        genesis.nNonce   = 47613;
+
+        hashGenesisBlock = genesis.GetHash();
+        assert(hashGenesisBlock == uint256("0x00003e92b3e0975b52fad48c2381e593d05e09da1452e9cd994819cc4ab8a71e"));
+        assert(genesis.hashMerkleRoot == uint256("0x5fbdf2bcf56b15f10ed9cf957f7543d55889b4f91e570408fa7220e0e2a79050"));
+
+        vFixedSeeds.clear();
+        vSeeds.clear();
+
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,48); // L
+        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,23);
+        base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,25);
+        base58Prefixes[STEALTH_ADDRESS] = std::vector<unsigned char>(1,43);
+        base58Prefixes[EXT_PUBLIC_KEY] = list_of(0x98)(0x74)(0x44)(0xE1).convert_to_container<std::vector<unsigned char> >();
+        base58Prefixes[EXT_SECRET_KEY] = list_of(0x98)(0x72)(0x42)(0xE2).convert_to_container<std::vector<unsigned char> >();
+
+        convertSeeds(vFixedSeeds, pnTestnetSeed, ARRAYLEN(pnTestnetSeed), nDefaultPort);
+
+        nLastPOWBlock = 210000;
+    }
+    virtual Network NetworkID() const { return CChainParams::TESTNET; }
+};
+static CTestNetParams testNetParams;
+
+
+static CChainParams *pCurrentParams = &mainParams;
+
+const CChainParams &Params() {
+    return *pCurrentParams;
+}
+
+void SelectParams(CChainParams::Network network) {
+    switch (network) {
+        case CChainParams::MAIN:
+            pCurrentParams = &mainParams;
+            break;
+        case CChainParams::TESTNET:
+            pCurrentParams = &testNetParams;
+            break;
+        default:
+            assert(false && "Unimplemented network");
+            return;
+    }
+}
+
+bool SelectParamsFromCommandLine() {
+
+    bool fTestNet = GetBoolArg("-testnet", false);
+
+    if (fTestNet) {
+        SelectParams(CChainParams::TESTNET);
+    } else {
+        SelectParams(CChainParams::MAIN);
+    }
+    return true;
+}
