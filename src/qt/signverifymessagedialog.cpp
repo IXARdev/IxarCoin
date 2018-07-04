0fe34348f9f33036fdf04d7ad0224a83ddb2728c
rugerpunk <40772252+rugerpunk@users.noreply.github.com>
2018-07-04 15:50:01 +0300

    Cosmetic change for Ixarcoin address prefix

--- src/qt/signverifymessagedialog.cpp
+++ src/qt/signverifymessagedialog.cpp
@@ -24,10 +24,10 @@ SignVerifyMessageDialog::SignVerifyMessageDialog(QWidget *parent) :
 
 #if (QT_VERSION >= 0x040700)
     /* Do not move this to the XML file, Qt before 4.7 will choke on it */
-    ui->addressIn_SM->setPlaceholderText(tr("Enter a Ixarcoin address (e.g. B8gZqgY4r2RoEdqYk3QsAqFckyf9pRHN6i)"));
+    ui->addressIn_SM->setPlaceholderText(tr("Enter a Ixarcoin address (e.g. i8gZqgY4r2RoEdqYk3QsAqFckyf9pRHN6i)"));
     ui->signatureOut_SM->setPlaceholderText(tr("Click \"Sign Message\" to generate signature"));
 
-    ui->addressIn_VM->setPlaceholderText(tr("Enter a Ixarcoin address (e.g. B8gZqgY4r2RoEdqYk3QsAqFckyf9pRHN6i)"));
+    ui->addressIn_VM->setPlaceholderText(tr("Enter a Ixarcoin address (e.g. i8gZqgY4r2RoEdqYk3QsAqFckyf9pRHN6i)"));
     ui->signatureIn_VM->setPlaceholderText(tr("Enter Ixarcoin signature"));
 #endif
 
